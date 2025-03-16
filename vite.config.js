e4c37a2c5780feb16b7c47ad3240f071746f6fb2
Mawiignen Tony Mallen-Ntiador <tonymawiignen@gmail.com>
2025-03-15 22:31:25 -0400

    Portfolio>

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/my_portfolio",
+  base: "/mawiignen_portfolio",
 })
